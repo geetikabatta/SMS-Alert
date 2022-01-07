79744f00e580a1e6e90aff929abbde9254b3d104
geetikabatta <42935104+geetikabatta@users.noreply.github.com>
2022-01-07 00:04:35 -0800

    Add files via upload
    SMS alert app. Pending AWS SNS credentials

---
+++ src/main/java/com/example/sms/SMSClass.java
@@ -0,0 +1,31 @@
+package com.example.sms;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+
+public class SMSClass {
+
+	public static void main(String[] args) {
+		
+		//Used for authenticating to AWS
+				BasicAWSCredentials awsCreds = new BasicAWSCredentials("Access_Key", "Secret_Access_Key");
+				
+				//Create SNS Client
+				AmazonSNS snsClient = AmazonSNSClient
+		                .builder()
+		                .withRegion(Regions.US_WEST_2)
+		                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+		                .build();
+				
+				String SMSMessage = "Sent using AWS SNS";
+				String mobile = "+1778XXXX";//Enter your mobile number here
+				
+				snsClient.publish(new PublishRequest().withMessage(SMSMessage).withPhoneNumber(mobile));
+
+	}
+
+}
